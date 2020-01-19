8c0f9c97a1072e6343ff5b56a0a103c921bd02f7
André Corrêa <andre.correa@stone.com.br>
2020-01-19 20:38:42 -0300

    integrate with backend and refactor

--- src/App.js
+++ src/App.js
@@ -1,116 +1,46 @@
 import React, { useState, useEffect } from 'react';
+import api from './services/api';
+
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
-function App() {
-  const [latitude, setLatitude] = useState('');
-  const [longitude, setLongitude] = useState('');
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem'; // pega o index por default
 
+function App() {
+  const [devs, setDevs] = useState([]);
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
+    async function loadDevs() {
+      const response = await api.get('/devs');
 
-        setLatitude(latitude);
-        setLongitude(longitude);
-      },
-      (err) => {
-        console.log(err);
-      },
-      {
-        timeout: 30000,
-      }
-    );
+      setDevs(response.data);
+    }
+
+    loadDevs();
   }, []);
+
+  async function handleAddDev(data) {
+
+    const response = await api.post('/devs', data);
+
+    setDevs([...devs, response.data]);
+  }
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required value={latitude} />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required value={longitude}/>
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-
-        </form>
+        <DevForm onSubimit={handleAddDev} />
       </aside>
 
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/41172399?s=460&v=4" alt="teste" />
-              <div className="user-info">
-                <strong>André Corrêa</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-
-            <p>Engenheiro mecatrônico e desenvolvedor de guerrilha.</p>
-            <a href="https://github.com/andre-correa">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/41172399?s=460&v=4" alt="teste" />
-              <div className="user-info">
-                <strong>André Corrêa</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-
-            <p>Engenheiro mecatrônico e desenvolvedor de guerrilha.</p>
-            <a href="https://github.com/andre-correa">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/41172399?s=460&v=4" alt="teste" />
-              <div className="user-info">
-                <strong>André Corrêa</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-
-            <p>Engenheiro mecatrônico e desenvolvedor de guerrilha.</p>
-            <a href="https://github.com/andre-correa">Acessar perfil no Github</a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/41172399?s=460&v=4" alt="teste" />
-              <div className="user-info">
-                <strong>André Corrêa</strong>
-                <span>ReactJS, React Native, Node.js</span>
-              </div>
-            </header>
-
-            <p>Engenheiro mecatrônico e desenvolvedor de guerrilha.</p>
-            <a href="https://github.com/andre-correa">Acessar perfil no Github</a>
-          </li>
+          {devs.map(dev => {
+            return <DevItem key={dev._id} dev={dev} />
+          })}
         </ul>
       </main>
     </div>
